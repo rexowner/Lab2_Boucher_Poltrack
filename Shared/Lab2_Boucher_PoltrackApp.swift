bc69c380ec61eab70ef8ac5820c849a4c0d873c7
Les Poltrack <lapwack@gmail.com>
2021-10-16 22:47:01 -0700

    Initial Commit

---
+++ Shared/Lab2_Boucher_PoltrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_Boucher_PoltrackApp.swift
+//  Shared
+//
+//  Created by Les Poltrack on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Lab2_Boucher_PoltrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
